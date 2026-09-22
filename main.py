fe2c3874c808bd46a8f49eed3ac7d0ba3d43dcac
clusko29-hash <clusko29@setoncatholic.org>
2026-09-22 20:17:24 +0000

    Number Guess Game (Bounes)

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import random
 
 
-game_number = random.randint(1,10)
-# print(game_number)
-while(True):
+game_number = random.randint(1, 10)
+guess_count = 0
+
+while True:
     guess = int(input("Guess a number 1-10: "))
+    guess_count += 1
+
     if guess > game_number:
         print("Too High")
     elif guess < game_number:
         print("Too Low")
     else:
-        print("You Win")
+        print(f"It took you {guess_count} guesses.")
+        if guess_count == 1:
+            print("your cheating")
+        elif guess_count <= 3:
+            print("good job.")
+        else:
+            print("that is awful.")
         break
