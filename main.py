1933fdeddee1d896b9afd006c7ef44cb7b5b8517
clusko29-hash <clusko29@setoncatholic.org>
2026-09-22 20:12:18 +0000

    Loop Game (Finish)

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import random
 
 
 game_number = random.randint(1,10)
-print(game_number)
+# print(game_number)
 while(True):
     guess = int(input("Guess a number 1-10: "))
     if guess > game_number:
